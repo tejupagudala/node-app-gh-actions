9f79df6b1d5b97ac221ac754665318ae129aa528
Sai tejasvi pagudala <sai@macbookpro.mynetworksettings.com>
2026-06-26 11:38:23 -0500

    chnaged the region

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "tejupagudala-node-app-tfstate-589077667712"
     key          = "hire/node-app/terraform.tfstate"
-    region       = "us-east-2"
+    region       = "ap-south-2"
     encrypt      = true
     use_lockfile = true
   }
